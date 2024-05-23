702139067211fd7db39abc61e7de9c9fbd0a02fc
Ian Bell <IanBell702@gmail.com>
2024-05-22 19:11:46 -0700

    Created TaskList

---
+++ frontend/src/components/TaskList.js
@@ -0,0 +1,21 @@
+import TaskItem from "@/components/TaskItem";
+
+export default function TaskList({ tasks, actions }) {
+    return (
+        <tbody>
+            {tasks.length === 0 ? (
+                <tr>
+                    <td colSpan="5">No tasks available</td>
+                </tr>
+            ) : (
+                tasks.map(task => (
+                    <TaskItem
+                        key={task.id}
+                        task={task}
+                        actions={actions}
+                    />
+                ))
+            )}
+        </tbody>
+    );
+}
